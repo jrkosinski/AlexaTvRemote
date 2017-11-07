b27eac2727c072a10b882059f3328bfb41c9aab9
john kosinski <j.r.kosinski@gmail.com>
2017-11-07 17:25:10 +0700

    added debug blinking

--- AlexaTvRemote.ino
+++ AlexaTvRemote.ino
@@ -1,32 +1,34 @@
 
 #include "debug.h"              // Serial debugger printing
 #include "WifiConnection.h"     // Wifi connection 
-//#include "Wemulator.h"          // Our Wemo emulator 
 #include "Wemulator.h"
 #include "WemoCallbackHandler.h"
 #include <IRsend.h>    // IR library 
 
 
 WifiConnection* wifi;           // wifi connection
-//Wemulator* wemulator;           // wemo emulator
 IRsend* irSend;                 // infrared sender
-Wemulator* wemo;
+Wemulator* wemulator;           // wemo emulator
+bool setupDone = false;
 
 //This is used as a crude workaround for a threading issue
 bool commandReceived = false;   // command flag
 
 //SET YOUR WIFI CREDS 
-const char* myWifiSsid      = "****"; 
-const char* myWifiPassword  = "*********";
+const char* myWifiSsid      = "mina"; 
+const char* myWifiPassword  = "HappyTime";
 
 //SET TO MATCH YOUR HARDWARE 
 #define SERIAL_BAUD_RATE    9600
 
 //PIN 0 is D3 ON THE CHIP 
 #define IR_PIN              0
+#define LED_PIN             2
 
 //turn on/off the tv by sending IR command
 void toggleTv();
+void doSetup();
+void blinkLed(int, int);
 
 
 /*---------------------------------------*/
@@ -36,6 +38,15 @@ void setup()
   //if set wrong, your serial debugger will not be readable 
   Serial.begin(SERIAL_BAUD_RATE);
   
+  debugPrintln("setting pin mode");
+  //pinMode(LED_PIN, OUTPUT);
+  //digitalWrite(LED_PIN, HIGH);
+
+  doSetup();
+}
+
+void doSetup()
+{
   //initialize wifi connection 
   wifi = new WifiConnection(myWifiSsid, myWifiPassword); 
   wifi->begin(); 
@@ -45,35 +56,17 @@ void setup()
   irSend->begin();
 
   //initialize wemo emulator 
-  //wemulator = new Wemulator(); 
-  wemo = new Wemulator(); 
+  wemulator = new Wemulator(); 
 
   //connect to wifi 
   if (wifi->connect())
   {
-    wemo->begin();
+    wemulator->begin();
     
-    wemo->addDevice("tv", 80, new WemoCallbackHandler(&commandReceived)); 
-    wemo->addDevice("television", 80, new WemoCallbackHandler(&commandReceived)); 
+    wemulator->addDevice("tv", 80, new WemoCallbackHandler(&commandReceived)); 
+    wemulator->addDevice("television", 80, new WemoCallbackHandler(&commandReceived)); 
 
-    /*
-    //start the wemo emulator (it runs as a series of webservers) 
-    wemulator->addCommand("tv"); 
-    wemulator->addCommand("television"); 
-    wemulator->addCommand("my tv"); 
-    wemulator->addCommand("my television"); 
-
-    //set the event handler for when a voice command is received 
-    wemulator->onMessage([](unsigned char device_id, const char * device_name, bool state) 
-    {
-      //the following is just to show that we don't care about state (i.e "on" or "off")
-      //since the command works as a toggle, it's the same command regardless of whether we say "turn on" or "turn off" 
-      if (state)
-        commandReceived = true;
-      else
-        commandReceived = true;
-    });
-    */
+    setupDone = true;
   }
 }
 
@@ -82,11 +75,14 @@ void setup()
 //Runs constantly 
 void loop() 
 {
+  if (!setupDone)
+    doSetup(); 
+    
   //let the wemulator listen for voice commands 
   if (wifi->isConnected)
   {
-    //wemulator->handle();
-    wemo->listen();
+    //blinkLed(1, 100);
+    wemulator->listen();
   }
 
   //if we've received a command, do the action 
@@ -104,7 +100,19 @@ void loop()
 //turn on/off the tv by sending IR command. This one is set for LG tv. 
 void toggleTv()
 {
-  DEBUG_PRINTLN("Sending IR command"); 
+  debugPrintln("Sending IR command"); 
   irSend->sendLG(0x00FFE01FUL, 0); 
+}
+
+void blinkLed(int count, int delayMs)
+{
+  for(int n=0; n<count; n++)
+  {
+    delay(delayMs);
+    debugPrintln("blink");
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
+    delay(delayMs);
+  }
 }
 
